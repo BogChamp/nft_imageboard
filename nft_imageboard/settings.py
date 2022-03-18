ef74eeb63042e658e3a3cc9f772d728ed7888074
bogdan <bogdan_aleksandrov_2014@mail.ru>
2022-03-18 21:08:02 +0300

    added captcha

--- nft_imageboard/settings.py
+++ nft_imageboard/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "aa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'imageboard',
+    'captcha',
 ]
 
 MIDDLEWARE = [
